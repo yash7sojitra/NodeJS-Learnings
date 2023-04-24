38e55d21b76c76da6e890b6fd587f9fce4de1dcd
yash7sojitra <yash.sojitra2002@gmail.com>
2023-04-24 22:50:41 +0530

    timestamps and filter tasks

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -17,10 +17,20 @@ router.post("/tasks", auth, async (req, res) => {
 });
 
 //Read all tasks of a user
+//GET /tasks?completed=true
 router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
+  }
+
   try {
-    const tasks = await Task.find({ owner: req.user._id });
-    res.send(tasks);
+    await req.user.populate({
+      path: "tasks",
+      match,
+    });
+    res.send(req.user.tasks);
   } catch (error) {
     res.status(500).send(error);
   }
