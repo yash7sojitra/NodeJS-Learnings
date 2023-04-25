cbc680807e8f971970fe4eba5355c7983ef28a6b
yash7sojitra <yash.sojitra2002@gmail.com>
2023-04-25 21:58:55 +0530

    filtration pagination and sorting

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -18,17 +18,30 @@ router.post("/tasks", auth, async (req, res) => {
 
 //Read all tasks of a user
 //GET /tasks?completed=true
+//GET /tasks?limit=10&skip=20
+//GET /tasks?sortBy=createdAt:desc
 router.get("/tasks", auth, async (req, res) => {
   const match = {};
+  const sort = {};
 
   if (req.query.completed) {
     match.completed = req.query.completed === "true";
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split("_");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
   }
 
   try {
     await req.user.populate({
       path: "tasks",
       match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort,
+      },
     });
     res.send(req.user.tasks);
   } catch (error) {
